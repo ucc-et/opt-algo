e6f1a4c843d876a58351b4226977464230cee7e0
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-12-12 01:08:24 +0100

    IMplement some base for the algorithms. too tired to do testing rn tho

--- new_approach/algorithms.py
+++ new_approach/algorithms.py
@@ -1,8 +1,63 @@
+from problem import OptimizationProblem
+
 class Greedy:
-    pass
+    def __init__(self, problem: OptimizationProblem, strategy):
+        self.problem = problem
+        self.strategy = strategy
+    
+    def solve(self):
+        current_solution = self.problem.start_solution()
+        best_solution = current_solution
+        best_value = self.problem.evaluate_solution(best_solution)
+
+        while True:
+            neighbors = self.problem.generate_neighbors(current_solution)
+            best_neighbor = None
+            best_neighbor_value = best_value
+
+            for neighbor in neighbors:
+                neighbor_value = self.problem.evaluate_solution(neighbor)
+                if neighbor_value > best_neighbor_value:
+                    best_neighbor = neighbor
+                    best_neighbor_value = neighbor_value
+
+            if best_neighbor is None:
+                break
+
+            current_solution = best_neighbor
+            if best_neighbor_value > best_value:
+                best_solution = best_neighbor
+                best_value = best_neighbor_value
+
+        return best_solution
 
 class LocalSearch:
-    pass
+    
+    def __init__(self, problem, neighborhood):
+        self.problem = problem
+        self.neighborhood = neighborhood
+        
+    def solve(self):
+        current_solution = self.problem.start_solution()
+        best_solution = current_solution
+        best_value = self.problem.evaluate_solution(best_solution)
+
+        while True:
+            neighbors = self.problem.generate_neighbors(current_solution)
+            improved = False
+            for neighbor in neighbors:
+                neighbor_value = self.problem.evaluate_solution(neighbor)
+                if neighbor_value > best_value:
+                    current_solution = neighbor
+                    best_solution = neighbor
+                    best_value = neighbor_value
+                    improved = True
+                    break
+
+            if not improved:
+                break
+
+        return best_solution
 
 class SimmulatedAnnealing:
     pass
